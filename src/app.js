3aa9a67b13f13f494004af37b4ab744124017d31
ItzMeAthu <athulkrishnakt453@gmail.com>
2026-09-19 20:34:59 +0530

    expression explored

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use("/test", (req,res) => {
+app.get("/test", (req,res) => {
+    console.log(req.query);
     res.send("Server is listening");
 })
+
+app.get(/^\/ab.*cd$/, (req, res) => {
+    res.send("hi hello");
+});
 
 app.listen(3000, () => {
     console.log("Server is listening in port 3000...");
